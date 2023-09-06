15367b7bc0968cdab74da953345f5622e58a2e28
Mario Kirven <mariokirven.mk@gmail.com>
2023-09-05 23:04:22 -0500

    Remove unused Hydrated bloc initialization

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:pokedex/app/app.dart';
 import 'package:pokedex/bootstrap.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   bootstrap(() => const App());
 }
