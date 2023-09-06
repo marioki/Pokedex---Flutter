912675fcf08b642c811fe9607a3a886442564197
Mario Kirven <mariokirven.mk@gmail.com>
2023-09-05 23:10:21 -0500

    Wrap Pokemon list item widget with inkwell for navigation

--- lib/pokemon_list/view/pokemon_list_item_widget.dart
+++ lib/pokemon_list/view/pokemon_list_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pokedex/models/pokemon_data_response.dart';
 
 import 'type_chip_widget.dart';
@@ -9,28 +10,33 @@ class PokemonListItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Text('#${pokemon.id.toString()}'),
-        Image(
-          width: 100,
-          height: 100,
-          fit: BoxFit.contain,
-          image: NetworkImage(
-            pokemon.sprites!.frontDefault!,
+    return InkWell(
+      onTap: () {
+        context.go('/pokemon/${pokemon.id}');
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text('#${pokemon.id.toString()}'),
+          Image(
+            width: 100,
+            height: 100,
+            fit: BoxFit.contain,
+            image: NetworkImage(
+              pokemon.sprites!.frontDefault!,
+            ),
           ),
-        ),
-        Text(pokemon.name!),
-        Spacer(),
-        Row(
-          children: pokemon.pokemonTypes!
-              .map((e) => TypeChipWidget(
-                    pokemonType: e,
-                  ))
-              .toList(),
-        )
-      ],
+          Text(pokemon.name!),
+          Spacer(),
+          Row(
+            children: pokemon.pokemonTypes!
+                .map((e) => TypeChipWidget(
+                      pokemonType: e,
+                    ))
+                .toList(),
+          )
+        ],
+      ),
     );
   }
 }
